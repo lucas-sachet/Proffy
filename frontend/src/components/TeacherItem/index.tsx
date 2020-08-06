8d10a19b69b269e2d23c329de7a1a76d792141b0
Lucas <lucasachet@gmail.com>
2020-08-06 19:29:02 -0300

    TeacherList - backend

--- frontend/src/components/TeacherItem/index.tsx
+++ frontend/src/components/TeacherItem/index.tsx
@@ -3,34 +3,57 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+    
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://instagram.fcxj6-1.fna.fbcdn.net/v/t51.2885-19/s150x150/91792027_761365427736324_2015898296865783808_n.jpg?_nc_ht=instagram.fcxj6-1.fna.fbcdn.net&_nc_ohc=XLWE3chopJoAX9fC3JV&oh=8f98e195ae18f22823aa73f0c8c5f1bc&oe=5F538ABA" alt="Lucas Sacjet"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Lucas Sachet</strong>
-                    <span>Dev</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            <br/><br/>
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Price/hour 
-                    <strong>$50,00</strong>
+                    <strong>$ {teacher.cost},00</strong>
                 </p>
 
-                <button type="button">
+                <a 
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={createNewConnection} 
+                href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="whatsapp"/>
                     Get in touch
-                </button>
+                </a>
             </footer>
         </article> 
     );
--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import PageHeader from '../../components/PageHeader';
-
-
-import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import api from '../../services/api';
+
+import './styles.css';
 
 
 function TeacherList() {
+
+    const [teachers, setTeachers] = useState([]);
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeek_day] = useState('');
+    const [time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data);
+    }
     return (
         <div id="page-teacher-list" className="container">
            <PageHeader title="These are the available Proffys.">
                
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={searchTeachers}>
                 <Select 
                 name="subject" 
                 label="Subject" 
+                value={subject}
+                onChange={e => { setSubject(e.target.value) }}
                 options={[
                     { value: 'Art', label: 'Art' },
                     { value: 'Biology', label: 'Biology' },
@@ -38,7 +59,9 @@ function TeacherList() {
                 />
                 <Select 
                 name="week_day" 
-                label="Day of the week" 
+                label="Day of the week"
+                value={week_day}
+                onChange={e => { setWeek_day(e.target.value) }} 
                 options={[
                     { value: '0', label: 'Monday' },
                     { value: '1', label: 'Tuesday' },
@@ -51,15 +74,24 @@ function TeacherList() {
                 ]}
                 />
                    
-                <Input type="time" name="time" label="Time" />
+                <Input 
+                type="time" 
+                name="time" 
+                label="Time"
+                value={time}
+                onChange={e => { setTime(e.target.value) }}
+                />
+
+                <button type='submit'>
+                    Search
+                </button>
                 </form>
             </PageHeader>
 
             <main>
-             <TeacherItem />
-             <TeacherItem />
-             <TeacherItem />
-             <TeacherItem />
+                {teachers.map((teacher :Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </main> 
         </div>
     )
